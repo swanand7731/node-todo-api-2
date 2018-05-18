bb32a496e438f16e823e2884cadec70d018ab2fa
swanand7731 <swanand7731@gmail.com>
2018-05-18 08:36:06 +0530

    Add uri changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://curious_mlab_user:Passw0rd@ds227740.mlab.com:27740/todoapp' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<username>:<password>@ds227740.mlab.com:27740/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
