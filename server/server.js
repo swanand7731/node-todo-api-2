8c413778842cbdd1c671b27678a7e2963dea31d1
swanand7731 <swanand7731@gmail.com>
2018-05-19 16:24:30 +0530

    Add delete todos by ID route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,6 +46,20 @@ app.get('/todos/:id', (req, res)=>{
     }, (err)=>{
         res.status(400).send(err);
     });
+});
+
+app.delete('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo)
+      return res.status(404).send();
+      res.send(todo);
+  }).catch((err)=>{
+    res.status(400).send();
+  });
 });
 
 app.listen(port, ()=>{
