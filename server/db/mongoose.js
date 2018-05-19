f8058f306e575547fae2b74af2cd3fc879f675aa
swanand7731 <swanand7731@gmail.com>
2018-05-19 16:36:37 +0530

    changes to mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://ghfdhfhfghfdhhfghdfg:asdfasdfasdf@ds227740.mlab.com:27740/todoapp' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
